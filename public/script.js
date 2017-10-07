e7ef16270727de69cd6d02297c077991632e005c
Rozelin <rozumna.olena@gmail.com>
2017-10-07 19:32:49 +0300

    Part 1 finished

--- public/script.js
+++ public/script.js
@@ -1,18 +1,28 @@
+var PRICE = 9.99;
+var LOAD_NUM = 10;
+
 new Vue({
 	el: '#app',
 	data: {
 		total: +0,
-		items: [
-			{ id: 1, title: "Item 1", price: 12.33 },
-			{ id: 2, title: "Item 2", price: 7.67 },
-			{ id: 3, title: "Item 3", price: 21.34 }
-		],
+		items: [],
+		resultNum: null,
 		cart: [],
-		search: ''
+		search: 'love',
+		lastSearch: '',
+		loading: false,
+		price: PRICE
 		},
 	methods: {
+		appendItems: function() {
+			if (this.items.length < this.result.length) {
+				var append = this.result.slice(this.items.length, this.items.length + LOAD_NUM);
+				this.items = this.items.concat(append);
+			}
+
+		},
 		addItem: function(index) {
-			this.total += this.items[index].price;
+			this.total += PRICE;
 			var item = this.items[index];
 			var found = false;
 			for (var i = 0; i < this.cart.length; i++) {
@@ -24,7 +34,7 @@ new Vue({
 			}
 			if (!found) {
 				this.cart.push({
-					price: item.price,
+					price: PRICE,
 					id: item.id,
 					title: item.title,
 					qty: 1
@@ -48,10 +58,19 @@ new Vue({
 			}
 		},
 		onSubmit: function() {
+			this.result = [];
+			this.items = [];
+			this.loading = true;
 			this.$http
-			.get('/search/'.concat('90s'))
+			.get('/search/'.concat(this.search))
 			.then(function(res) {
-				console.log(res);
+				this.result = res.data;
+				this.resultNum = res.data.length;
+				this.appendItems();
+				this.lastSearch = this.search;
+				this.loading = false;
+			}, function(res) {
+				this.loading = false;
 			});
 		}
 	},
@@ -59,5 +78,15 @@ new Vue({
 		currency: function(price) {
 			return '$'.concat(price.toFixed(2));
 		}
+	},
+	mounted: function() {
+		this.onSubmit();
+
+		var vueInstance = this;
+		var scrolElem = document.getElementById('product-list-bottom');
+		var watcher = scrollMonitor.create(scrolElem);
+		watcher.enterViewport(function() {
+			vueInstance.appendItems();
+		});
 	}
 });
